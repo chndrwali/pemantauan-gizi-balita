045d470fa8670f0eadad56923d5bf0a309edfaba
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2025-11-19 21:42:11 +0700

    add

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
+
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   // experimental: {
   //   turbopackFileSystemCacheForDev: true,
   // },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
